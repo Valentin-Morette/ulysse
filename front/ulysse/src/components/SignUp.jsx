dd5a65c67f5fc5686a04be747dd452384f36ac97
MORETTE Valentin <morette.valentin@gmail.com>
2022-07-11 11:51:09 +0200

    morette_valentin_quete020

--- front/ulysse/src/components/SignUp.jsx
+++ front/ulysse/src/components/SignUp.jsx
@@ -4,18 +4,59 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.updateEmailField = this.updateEmailField.bind(this);
-    this.state = { email: "" };
+    this.updateNameField = this.updateNameField.bind(this);
+    this.updateLastnameField = this.updateLastnameField.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      email: "valentin.morette@gmail.com",
+      name: "Valentin",
+      lastname: "Morette",
+      password: "",
+      passwordbis: "",
+    };
   }
 
   updateEmailField(event) {
     this.setState({ email: event.target.value });
   }
 
+  updateNameField(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  updateLastnameField(event) {
+    this.setState({ lastname: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div>
-        <h1>{this.state.email}</h1>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
         <input type="email" name="email" onChange={this.updateEmailField} />
+        <p>
+          Password : <input type="password" name="password" />
+        </p>
+        <p>
+          Password bis : <input type="password" name="passwordbis" />
+        </p>
+        <p>
+          Name :{" "}
+          <input type="text" name="name" onChange={this.updateNameField} />
+        </p>
+        <p>
+          Lastname :{" "}
+          <input
+            type="text"
+            name="lastname"
+            onChange={this.updateLastnameField}
+          />
+        </p>
+        <input type="submit" value="Soumettre" onClick={this.handleSubmit} />
       </div>
     );
   }
